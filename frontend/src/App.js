fd5246719b0e8ed3bc72e8548d6cae263a88109f
shiva <shiwasingh4@gmail.com>
2026-04-16 10:43:21 +0530

    hide psycheguide on chat page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
 import './styles/global.css';
 
@@ -11,22 +11,30 @@ import DocsPage from './pages/DocsPage';
 import PersonalityQuizPage from './pages/PersonalityQuizPage';
 import PsycheGuide from './components/PsycheGuide';
 
+function AppContent() {
+  const location = useLocation();
+  const hideChatbot = location.pathname === '/chat';
+
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/docs" element={<DocsPage />} />
+        <Route path="/personality-quiz" element={<PersonalityQuizPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      {!hideChatbot && <PsycheGuide />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/result" element={<ResultPage />} />
-          <Route path="/chat" element={<ChatPage />} />
-          <Route path="/docs" element={<DocsPage />} />
-          <Route path="/personality-quiz" element={<PersonalityQuizPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        {/* Global floating chatbot - visible on all pages */}
-        <PsycheGuide />
-      </div>
+      <AppContent />
     </Router>
   );
 }
